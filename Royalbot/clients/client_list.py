b9645d683c562b1ac488fef766c7a1db2d755d2e
legendxdxx <105597001+legendxdxx@users.noreply.github.com>
2022-07-07 20:11:33 +0530

    Update and rename hellbot/clients/client_list.py to Royalbot/clients/client_list.py

--- Royalbot/clients/client_list.py
+++ Royalbot/clients/client_list.py
@@ -1,8 +1,8 @@
 from telethon.utils import get_peer_id
 from telethon.tl.functions.users import GetFullUserRequest
 
-from .session import Hell, H2, H3, H4, H5
-from hellbot.sql.gvar_sql import gvarstat
+from .session import Royal, R2, R3, R4, R5
+from royalbot.sql.gvar_sql import gvarstat
 
 
 async def clients_list():
@@ -12,33 +12,33 @@ async def clients_list():
         for b in a:
             c = int(b)
             user_ids.append(c)
-    main_id = await Hell.get_me()
+    main_id = await royal.get_me()
     user_ids.append(main_id.id)
 
     try:
         if H2 is not None:
-            id2 = await H2.get_me()
+            id2 = await R2.get_me()
             user_ids.append(id2.id)
     except:
         pass
 
     try:
         if H3 is not None:
-            id3 = await H3.get_me()
+            id3 = await R3.get_me()
             user_ids.append(id3.id)
     except:
         pass
 
     try:
         if H4 is not None:
-            id4 = await H4.get_me()
+            id4 = await R4.get_me()
             user_ids.append(id4.id)
     except:
         pass
 
     try:
         if H5 is not None:
-            id5 = await H5.get_me()
+            id5 = await R5.get_me()
             user_ids.append(id5.id)
     except:
         pass
@@ -50,12 +50,12 @@ async def client_id(event, botid=None):
     if botid is not None:
         uid = await event.client(GetFullUserRequest(botid))
         ForGo10God = uid.user.id
-        HELL_USER = uid.user.first_name
-        hell_mention = f"[{HELL_USER}](tg://user?id={ForGo10God})"
+        Royal_USER = uid.user.first_name
+        royal_mention = f"[{Royal_USER}](tg://user?id={ForGo10God})"
     else:
         client = await event.client.get_me()
         uid = get_peer_id(client)
         ForGo10God = uid
-        HELL_USER = client.first_name
-        hell_mention = f"[{HELL_USER}](tg://user?id={ForGo10God})"
-    return ForGo10God, HELL_USER, hell_mention
+        Royal_USER = client.first_name
+        royal_mention = f"[{Royal_USER}](tg://user?id={ForGo10God})"
+    return ForGo10God, Royal_USER, royal_mention
