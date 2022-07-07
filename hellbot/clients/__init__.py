f50508564c7b8ca6528d84f2294ee31cb7a96e68
legendxdxx <105597001+legendxdxx@users.noreply.github.com>
2022-07-07 20:08:18 +0530

    Update __init__.py

--- hellbot/clients/__init__.py
+++ hellbot/clients/__init__.py
@@ -1,3 +1,3 @@
 from .client_list import clients_list, client_id
-from .decs import hell_cmd, hell_handler
-from .session import Hell, H2, H3, H4, H5, HellBot
+from .decs import royal_cmd, royal_handler
+from .session import Royal, R2, R3, R4, R5, Royalbot
