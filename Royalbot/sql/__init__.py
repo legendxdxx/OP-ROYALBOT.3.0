09831a29a21c3fbc42bba388c4d343ce7809002f
legendxdxx <105597001+legendxdxx@users.noreply.github.com>
2022-07-08 17:29:25 +0530

    Update and rename hellbot/sql/__init__.py to Royalbot/sql/__init__.py

--- Royalbot/sql/__init__.py
+++ Royalbot/sql/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-# from hellbot.config import Config
+# from royalbot.config import Config
 
 DB_URI = os.environ.get("DATABASE_URL")
 
