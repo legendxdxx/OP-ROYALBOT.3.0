4bab470264dc735ca239524c847d30a5a1462725
legendxdxx <105597001+legendxdxx@users.noreply.github.com>
2022-07-08 12:25:21 +0530

    Update hell_string.py

--- hell_string.py
+++ hell_string.py
@@ -6,26 +6,26 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 
-print("•••   HELLBOT  SESSION  GENERATOR   •••")
-print("\nHello!! Welcome to HellBot Session Generator\n")
+print("•••   ROYALẞøT  SESSION  GENERATOR   •••")
+print("\nHello!! Welcome to ROYALẞøT Session Generator\n")
 okvai = input("Enter 69 to continue: ")
 if okvai == "69":
-    print("Choose the string session type: \n1. HellBot \n2. Music Bot")
+    print("Choose the string session type: \n1. ROYALẞøT \n2. Music Bot")
     library = input("\nYour Choice: ")
     if library == "1":
-        print("\nTelethon Session For HellBot")
+        print("\nTelethon Session For ROYALẞøT")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
-        with TelegramClient(StringSession(), APP_ID, API_HASH) as hellbot:
-            print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
-            hellbot.send_message("me", f"#HELLBOT #HELLBOT_SESSION \n\n`{hellbot.session.save()}`")
+        with TelegramClient(StringSession(), APP_ID, API_HASH) as royalbot:
+            print("\nYour ROYALẞøT Session Is sent in your Telegram Saved Messages.")
+            hellbot.send_message("me", f"#ROYALBOT #ROYALBOT_SESSION \n\n`{royalbot.session.save()}`")
     elif library == "2":
         print("Pyrogram Session for Music Bot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
-        with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as hellbot:
-            print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
-            hellbot.send_message("me", f"#HELLBOT_MUSIC #HELLBOT_SESSION\n\n`{hellbot.export_session_string()}`")
+        with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as royalbot:
+            print("\nYour ROYALẞøT Session Is sent in your Telegram Saved Messages.")
+            royalbot.send_message("me", f"#ROYALBOT_MUSIC #ROYALBOT_SESSION\n\n`{royalbot.export_session_string()}`")
     else:
         print("Please Enter 1 or 2 only.")
 else:
