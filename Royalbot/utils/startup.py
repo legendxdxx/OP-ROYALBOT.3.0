211730e85a4daad10d4ea4427ac2e9caedc65b72
legendxdxx <105597001+legendxdxx@users.noreply.github.com>
2022-07-13 18:54:19 +0530

    Update and rename hellbot/utils/startup.py to Royalbot/utils/startup.py

--- Royalbot/utils/startup.py
+++ Royalbot/utils/startup.py
@@ -2,15 +2,15 @@ from telethon import Button
 from telethon.tl import functions
 from telethon.tl.types import ChatAdminRights
 
-from hellbot import LOGS
-from hellbot.config import Config
-from hellbot.helpers.int_str import make_int
-from hellbot.sql.gvar_sql import addgvar, gvarstat
+from royalbot import LOGS
+from royalbot.config import Config
+from royalbot.helpers.int_str import make_int
+from royalbot.sql.gvar_sql import addgvar, gvarstat
 
 
 # Creates the logger group on first deploy and adds the helper bot
 async def logger_id(client):
-    desc = "A Bot Logger Group For Hellbot. DO NOT LEAVE THIS GROUP!!"
+    desc = "A Bot Logger Group For ROYALẞØT. DO NOT LEAVE THIS GROUP!!"
     new_rights = ChatAdminRights(
         add_admins=True,
         invite_users=True,
@@ -21,7 +21,7 @@ async def logger_id(client):
         manage_call=True,
     )
     try:
-        grp = await client(functions.channels.CreateChannelRequest(title="Hellbot Logger", about=desc, megagroup=True))
+        grp = await client(functions.channels.CreateChannelRequest(title="ROYALẞØT Logger", about=desc, megagroup=True))
         grp_id = grp.chats[0].id
         grp = await client(functions.messages.ExportChatInviteRequest(peer=grp_id))
         await client(functions.channels.InviteToChannelRequest(channel=grp_id, users=[Config.BOT_USERNAME]))
@@ -63,24 +63,24 @@ async def start_msg(client, pic, version, total):
 <b><i>Clients :</b></i> <code>{str(total)}</code>
 <b><i>Sudo :</b></i> <code>{is_sudo}</code>
 
-<b><i>»» <u><a href='https://t.me/Its_HellBot'>†hê Hêllẞø†</a></u> ««</i></b>
+<b><i>»» <u><a href='https://t.me/royalyserbot'>ROYALẞØT</a></u> ««</i></b>
 """
     await client.send_file(
         Config.LOGGER_ID,
         pic,
         caption=text,
         parse_mode="HTML",
-        buttons=[[Button.url("HellBot Network", "https://t.me/HellBot_Network")]],
+        buttons=[[Button.url("ROYALẞØT Network", "https://t.me/RDNNETWORK")]],
     )
 
 
-# Joins the hellbot chat and channel from all clients
+# Joins the ROYAL-BOT chat and channel from all clients
 async def join_it(client):
     if client:
         try:
-            await client(functions.channels.JoinChannelRequest("@Its_HellBot"))
+            await client(functions.channels.JoinChannelRequest("@ROYALYSERBOT"))
             await client(functions.messages.ImportChatInviteRequest('bZxlmdNFp1NjMDNh'))
         except BaseException:
             pass
 
-# hellbot
+# royalbot
