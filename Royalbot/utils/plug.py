ef7115fdc7da774f5b0a530e1dcd6dba86c776f6
legendxdxx <105597001+legendxdxx@users.noreply.github.com>
2022-07-13 18:45:08 +0530

    Update and rename hellbot/utils/plug.py to Royalbot/utils/plug.py

--- Royalbot/utils/plug.py
+++ Royalbot/utils/plug.py
@@ -16,17 +16,17 @@ from telethon import events
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator, InputMessagesFilterDocument
 
-from hellbot import *
-from hellbot.clients import *
-from hellbot.helpers import *
-from hellbot.config import *
-from hellbot.utils import *
+from royalbot import *
+from royalbot.clients import *
+from royalbot.helpers import *
+from royalbot.config import *
+from royalbot.utils import *
 
 
 # ENV
 ENV = bool(os.environ.get("ENV", False))
 if ENV:
-    from hellbot.config import Config
+    from royalbot.config import Config
 else:
     if os.path.exists("Config.py"):
         from Config import Development as Config
@@ -37,57 +37,57 @@ def load_module(shortname):
     if shortname.startswith("__"):
         pass
     elif shortname.endswith("_"):
-        import hellbot.utils
+        import royalbot.utils
 
         path = Path(f"hellbot/plugins/{shortname}.py")
-        name = "hellbot.plugins.{}".format(shortname)
+        name = "royalbot.plugins.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        LOGS.info("HellBot - Successfully imported " + shortname)
+        LOGS.info("Royalbot - Successfully imported " + shortname)
     else:
         import hellbot.utils
 
         path = Path(f"hellbot/plugins/{shortname}.py")
-        name = "hellbot.plugins.{}".format(shortname)
+        name = "royalbot.plugins.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
-        mod.bot = Hell
-        mod.H1 = Hell
-        mod.H2 = H2
-        mod.H3 = H3
-        mod.H4 = H4
-        mod.H5 = H5
-        mod.Hell = Hell
-        mod.HellBot = HellBot
-        mod.tbot = HellBot
+        mod.bot = Royal
+        mod.R1 = Royal
+        mod.R2 = R2
+        mod.R3 = R3
+        mod.R4 = R4
+        mod.R5 = R5
+        mod.Royal = Royal
+        mod.Royalbot = Royalbot
+        mod.tbot = RoyalBot
         mod.tgbot = bot.tgbot
         mod.command = command
         mod.CmdHelp = CmdHelp
         mod.client_id = client_id
         mod.logger = logging.getLogger(shortname)
         # support for uniborg
-        sys.modules["uniborg.util"] = hellbot.utils
+        sys.modules["uniborg.util"] = royalbot.utils
         mod.Config = Config
         mod.borg = bot
-        mod.hellbot = bot
+        mod.royalbot = bot
         mod.edit_or_reply = edit_or_reply
         mod.eor = edit_or_reply
-        mod.delete_hell = delete_hell
-        mod.eod = delete_hell
+        mod.delete_royal = delete_royal
+        mod.eod = delete_royal
         mod.Var = Config
         mod.admin_cmd = admin_cmd
-        mod.hell_cmd = hell_cmd
+        mod.royal_cmd = royal_cmd
         mod.sudo_cmd = sudo_cmd
         # support for other userbots
-        sys.modules["userbot.utils"] = hellbot.utils
-        sys.modules["userbot"] = hellbot
+        sys.modules["userbot.utils"] = royalbot.utils
+        sys.modules["userbot"] = royalbot
         # support for paperplaneextended
-        sys.modules["userbot.events"] = hellbot
+        sys.modules["userbot.events"] = royalbot
         spec.loader.exec_module(mod)
         # for imports
-        sys.modules["hellbot.plugins." + shortname] = mod
-        LOGS.info("⚡ Hêllẞø† ⚡ - Successfully Imported " + shortname)
+        sys.modules["royalbot.plugins." + shortname] = mod
+        LOGS.info("⚡ ROYALBOT ⚡ - Successfully Imported " + shortname)
 
 
 # remove plugins
@@ -99,7 +99,7 @@ def remove_plugin(shortname):
             del LOAD_PLUG[shortname]
 
         except BaseException:
-            name = f"hellbot.plugins.{shortname}"
+            name = f"royalbot.plugins.{shortname}"
 
             for i in reversed(range(len(bot._event_builders))):
                 ev, cb = bot._event_builders[i]
@@ -111,18 +111,18 @@ def remove_plugin(shortname):
 
 async def plug_channel(client, channel):
     if channel:
-        LOGS.info("⚡ Hêllẞø† ⚡ - PLUGIN CHANNEL DETECTED.")
-        LOGS.info("⚡ Hêllẞø† ⚡ - Starting to load extra plugins.")
+        LOGS.info("⚡ ROYALẞØT ⚡ - PLUGIN CHANNEL DETECTED.")
+        LOGS.info("⚡ ROYALẞØT ⚡ - Starting to load extra plugins.")
         plugs = await client.get_messages(channel, None, filter=InputMessagesFilterDocument)
         total = int(plugs.total)
         for plugins in range(total):
             plug_id = plugs[plugins].id
             plug_name = plugs[plugins].file.name
-            if os.path.exists(f"hellbot/plugins/{plug_name}"):
+            if os.path.exists(f"royalbot/plugins/{plug_name}"):
                 return
             downloaded_file_name = await client.download_media(
                 await client.get_messages(channel, ids=plug_id),
-                "hellbot/plugins/",
+                "royalbot/plugins/",
             )
             path1 = Path(downloaded_file_name)
             shortname = path1.stem
@@ -132,4 +132,4 @@ async def plug_channel(client, channel):
                 LOGS.error(str(e))
 
 
-# hellbot
+# royalbot
